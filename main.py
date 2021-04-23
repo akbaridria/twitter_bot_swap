0f64ef194415ba62b13c69631529bcf37461fe48
akbaridria <akbaridria15@gmail.com>
2021-04-23 14:53:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ API_KEY = environ["API_KEY"]
 API_SECRET_KEY = environ["API_SECRET_KEY"]
 ACCESS_TOKEN = environ["ACCESS_TOKEN"]
 ACCESS_SECRET_TOKEN = environ["ACCESS_SECRET_TOKEN"]
-uniswap_block = '12273059'
+uniswap_block = '12295226'
 #make a connection to twitter
 def connect_to_twitter() :
   auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
